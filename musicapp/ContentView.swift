0221eea098e5886304f0cb1f382e35e536039e41
Anurag Singh <s.anurag1998@gmail.com>
2026-05-22 18:39:25 +0530

    Initial Commit

---
+++ musicapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  musicapp
+//
+//  Created by Anurag Singh on 22/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ musicapp/musicappApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicappApp.swift
+//  musicapp
+//
+//  Created by Anurag Singh on 22/05/26.
+//
+
+import SwiftUI
+
+@main
+struct musicappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
